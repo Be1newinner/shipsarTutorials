f153ea17d84da613895c065fff40005dbea3703e
vijay <be1newinner@gmail.com>
2022-09-07 16:47:21 +0530

    3

--- apis/index.js
+++ apis/index.js
@@ -1,17 +1,16 @@
-// Add Express
-import express from "express";
+const app = require('express')();
+import { v4 } from 'uuid';
 
-// Initialize Express
-const app = express();
-
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-// Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-module.exports = app;
+export default app;
